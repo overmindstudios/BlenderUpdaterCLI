7688f32f8aff91077712261a2dac03f23a70c24f
tobi <tobi@overmind-studios.de>
2018-10-29 15:09:27 +0100

    aaand it works

--- BlenderUpdaterCLI.py
+++ BlenderUpdaterCLI.py
@@ -1,87 +1,134 @@
 import argparse
 from colorama import init, Fore
 import platform
-from os import path
-import tqdm
+import os
+import shutil
+import requests
+import re
+from progress.bar import IncrementalBar
+import threading
+from distutils.dir_util import copy_tree # pylint: disable=no-name-in-module,import-error
+import sys
+import time
+
+class Spinner:
+    busy = False
+    delay = 0.1
+
+    @staticmethod
+    def spinning_cursor():
+        while 1: 
+            for cursor in '|/-\\': yield cursor
+
+    def __init__(self, title, delay=None):
+        self.spinner_generator = self.spinning_cursor()
+        if delay and float(delay): self.delay = delay
+        self.title = title
+
+    def spinner_task(self):
+        while self.busy:
+            sys.stdout.write(self.title + next(self.spinner_generator))
+            sys.stdout.flush()
+            time.sleep(self.delay)
+            sys.stdout.write('\b' * (len(self.title) + 1))
+            sys.stdout.flush()
+
+    def start(self):
+        self.busy = True
+        threading.Thread(target=self.spinner_task).start()
+
+    def stop(self):
+        self.busy = False
+        time.sleep(self.delay)
 
 init(autoreset=True)    # enable Colorama autoreset
 failed = False
+url = 'https://builder.blender.org/download/'
 
 parser = argparse.ArgumentParser(description="Update Blender to latest nightly build. (c) 2018 by Overmind Studios.", epilog="example usage: BlenderUpdaterCLI -p C:\\Blender -b 28")
 parser.add_argument('-p','--path', help="Destination path", required=True, type=str)
-parser.add_argument('-b', '--blender', help="Desired Blender version, either '-b 279' or '-b 28'", required=True, type=str)
-parser.add_argument('-a','--architecture', help="Architecture ('x86' or 'x64'). If omitted, it will autodetect current architecture.", required=False, type=str)
+parser.add_argument('-b','--blender', help="Desired Blender version, either '-b 279' or '-b 28'", required=True, type=str)
+parser.add_argument('-a','--architecture', help="Architecture ('x86' or 'x64'). If omitted, it will autodetect current architecture.", type=str)
 parser.add_argument('-o','--operatingsystem', help="Operating system. 'osx', 'linux' or 'windows'. If omitted, it will autodetect current OS.", type=str)
 parser.add_argument('-r','--run', help="Run downloaded Blender version when finished", action="store_true")
-parser.add_argument('-v', '--version', action='version', version='0.1', help="Print program version")
+parser.add_argument('-v','--version', action='version', version='0.1', help="Print program version")
 args = parser.parse_args()
 
 print(" SETTINGS ".center(80, "-"))
 
 # check for path validity
-if path.isdir(args.path):
+if os.path.isdir(args.path):
+    dir_ = args.path
     print("Destination path: " + Fore.GREEN + args.path)
 else:
-    print(Fore.RED + "'" + args.path + "'" + " is an invalid path, please make sure directory exists")
+    print(Fore.RED + "'" + args.path + "'" + " is an invalid path, make sure directory exists")
     failed = True
 
 # check for desired blender version
 if args.blender == "28":
-    blender = "2.8"
-    print("Blender version: " + Fore.GREEN + blender)
+    blender = "2.80"
+    print("Blender version: " + Fore.GREEN + "2.8")
 elif args.blender == "279":
     blender = "2.79"
-    print("Blender version: " + Fore.GREEN + blender)
+    print("Blender version: " + Fore.GREEN + "2.79")
 else:
-    print(Fore.RED + "Syntax error - please use '-b 279' for Blender 2.79 or '-b 28' for Blender 2.8")
+    print(Fore.RED + "Syntax error - use '-b 279' for Blender 2.79 or '-b 28' for Blender 2.8")
     failed = True
 
 # check for desired operating system or autodetect when empty
 if args.operatingsystem == "windows":
-    os = "Windows"
-    print("Operating system: " + Fore.GREEN + os)
+    opsys = "win"
+    extension = "zip"
+    print("Operating system: " + Fore.GREEN + "Windows")
 elif args.operatingsystem == "osx":
-    os = "OSX"
-    print("Operating system: " + Fore.GREEN + os)
+    opsys = "OSX"
+    extension = "zip"
+    print("Operating system: " + Fore.GREEN + "OSX")
 elif args.operatingsystem == "linux":
-    os = "Linux"
-    print("Operating system: " + Fore.GREEN + os)
+    opsys = "linux"
+    extension = "tar.bz2"
+    print("Operating system: " + Fore.GREEN + "Linux")
 elif not args.operatingsystem:
     if platform.system() == "Windows":
-        os = "Windows"
-        print("Operating system: " + Fore.GREEN + os + Fore.CYAN + " (autodetected)")
+        opsys = "win"
+        extension = "zip"
+        print("Operating system: " + Fore.GREEN + "Windows" + Fore.CYAN + " (autodetected)")
     elif platform.system() == "Linux":
-        os = "Linux"
-        print("Operating system: " + Fore.GREEN + os + Fore.CYAN + " (autodetected)")
+        opsys = "linux"
+        extension = "tar.bz2"
+        print("Operating system: " + Fore.GREEN + "Linux" + Fore.CYAN + " (autodetected)")
     elif platform.system() == "Darwin":
-        os = "OSX"
-        print("Operating system: " + Fore.GREEN + os + Fore.CYAN + " (autodetected)")
+        opsys = "OSX"
+        extension = "zip"
+        print("Operating system: " + Fore.GREEN + "OSX" + Fore.CYAN + " (autodetected)")
 else:
-    print(Fore.RED + "Syntax error - please use '-o windows', '-o linux' or '-o osx'")
+    print(Fore.RED + "Syntax error - use '-o windows', '-o linux' or '-o osx'")
     failed = True
 
 # check for desired architecture or autodetect when empty
 if args.architecture == "x86":
-    if os == "OSX":
+    if opsys == "OSX":
         print(Fore.RED + "Error - no 32bit build for OSX")
         failed = True
+    elif opsys == "linux":
+        arch = "686"
     else:
-        arch = "32bit"
-        print("Architecture: " + Fore.GREEN + arch)
+        arch = "32"
+        print("Architecture: " + Fore.GREEN + "32bit")
 elif args.architecture == "x64":
-    arch = "64bit"
-    print("Architecture: " + Fore.GREEN + arch)
+    arch = "64"
+    print("Architecture: " + Fore.GREEN + "64bit")
 elif not args.architecture:
     if "32" in platform.machine():
-        if os == "OSX":
+        if opsys == "OSX":
             print(Fore.RED + "Error - no 32bit build for OSX")
             failed = True
         else:
-            arch = "32bit"
-            print("Architecture: " + Fore.GREEN + arch + Fore.CYAN + " (autodetected)")
+            arch = "32"
+            print("Architecture: " + Fore.GREEN + "32bit" + Fore.CYAN + " (autodetected)")
     elif "64" in platform.machine():
-        arch = "64bit"
-        print("Architecture: " + Fore.GREEN + arch + Fore.CYAN + " (autodetected)")
+        arch = "64"
+        print("Architecture: " + Fore.GREEN + "64bit" + Fore.CYAN + " (autodetected)")
 else:
     print(Fore.RED + "Syntax error - please use '-a x86' for 32bit or '-a x64' for 64bit")
     failed = True
@@ -94,7 +141,6 @@ else:
     print(Fore.MAGENTA + "Will NOT run Blender when finished")
     will_run = False
 
-
 print("-".center(80, "-"))
 
 # Abort if any error occured during parsing
@@ -103,3 +149,43 @@ if failed == True:
     quit()
 else:
     print(Fore.GREEN + "All settings valid, proceeding...")
+    try:
+        req = requests.get(url)
+    except Exception:
+        print(Fore.RED + "Error connecting to " + url + ", check your internet connection")
+    
+    filename = re.findall('blender-' + blender + '-\w+-' + opsys + '[0-9a-zA-Z-._]*' + arch + '\.' + extension, req.text)
+
+    if os.path.isdir('./blendertemp'):
+        shutil.rmtree('./blendertemp')
+    os.makedirs('./blendertemp')
+    #dir_ = os.path.join(args.path, '')
+    print("Downloading " + filename[0])
+    chunkSize = 10240
+    r = requests.get(url + filename[0], stream=True)
+    
+    with open("./blendertemp/" + filename[0], 'wb') as f:
+        pbar = IncrementalBar('Downloading', max=int(r.headers['Content-Length']) / chunkSize, suffix='%(percent)d%%')
+        for chunk in r.iter_content(chunk_size=chunkSize): 
+            if chunk: # filter out keep-alive new chunks
+                pbar.next()
+                f.write(chunk)
+        pbar.finish()
+    print('Download ' + Fore.GREEN + 'done')
+
+    # Extraction
+    spinnerExtract = Spinner('Extracting... ')
+    spinnerExtract.start()
+    shutil.unpack_archive("./blendertemp/" + filename[0], './blendertemp/')
+    spinnerExtract.stop()
+    print('Extraction ' + Fore.GREEN + 'done')
+
+    # Copying
+    source = next(os.walk('./blendertemp/'))[1]
+    spinnerCopy = Spinner('Copying... ')
+    spinnerCopy.start()
+    copy_tree(os.path.join('./blendertemp/', source[0]), dir_)
+    spinnerCopy.stop()
+    print('Copying ' + Fore.GREEN + 'done')
+
+    print(Fore.GREEN + "All tasks finished")
