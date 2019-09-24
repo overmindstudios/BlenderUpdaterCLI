b0403d1a7d8d3f5923eaafd007849df126cb50de
Tobias Kummer <tobi@overmind-studios.de>
2019-09-25 01:20:52 +0200

    pep8 fixes

--- BlenderUpdaterCLI.py
+++ BlenderUpdaterCLI.py
@@ -1,4 +1,4 @@
-'''
+"""
     Overmind Studios BlenderUpdaterCLI - update Blender to latest buildbot version
     Copyright (C) 2018-2019 by Tobias Kummer for Overmind Studios
 
@@ -14,7 +14,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
 
 from colorama import init, Fore
 from distutils.dir_util import copy_tree
@@ -32,9 +32,9 @@ import threading
 import time
 
 
-init(autoreset=True)    # enable Colorama autoreset
+init(autoreset=True)  # enable Colorama autoreset
 failed = False
-url = 'https://builder.blender.org/download/'
+url = "https://builder.blender.org/download/"
 config = configparser.ConfigParser()
 
 
@@ -45,7 +45,7 @@ class Spinner:
     @staticmethod
     def spinning_cursor():
         while 1:
-            for cursor in '|/-\\':
+            for cursor in "|/-\\":
                 yield cursor
 
     def __init__(self, title, delay=None):
@@ -59,7 +59,7 @@ class Spinner:
             sys.stdout.write(self.title + next(self.spinner_generator))
             sys.stdout.flush()
             time.sleep(self.delay)
-            sys.stdout.write('\b' * (len(self.title) + 1))
+            sys.stdout.write("\b" * (len(self.title) + 1))
             sys.stdout.flush()
 
     def start(self):
@@ -71,15 +71,41 @@ class Spinner:
         time.sleep(self.delay)
 
 
-parser = argparse.ArgumentParser(description="Update Blender to latest nightly build. (c) 2018-2019 by Tobias Kummer/Overmind Studios.", epilog="example usage: BlenderUpdaterCLI -p C:\\Blender")
-parser.add_argument('-p', '--path', help="Destination path", required=True, type=str)
-parser.add_argument('-a', '--architecture', help="Architecture ('x86' or 'x64'). If omitted, it will autodetect current architecture.", type=str)
-parser.add_argument('-o', '--operatingsystem', help="Operating system. 'osx', 'linux' or 'windows'. If omitted, it will try to autodetect current OS.", type=str)
-parser.add_argument('-y', '--yes', help="Install even if version already installed", action="store_true")
-parser.add_argument('-n', '--no', help="Don't install if version already installed", action="store_true")
-parser.add_argument('-k', '--keep', help="Keep temporary downloaded archive file", action="store_true")
-parser.add_argument('-r', '--run', help="Run downloaded Blender version when finished", action="store_true")
-parser.add_argument('-v', '--version', action='version', version='1.2', help="Print program version")
+parser = argparse.ArgumentParser(
+    description="Update Blender to latest nightly build. (c) 2018-2019 by Tobias Kummer/Overmind Studios.",
+    epilog="example usage: BlenderUpdaterCLI -p C:\\Blender",
+)
+parser.add_argument("-p", "--path", help="Destination path", required=True, type=str)
+parser.add_argument(
+    "-a",
+    "--architecture",
+    help="Architecture ('x86' or 'x64'). If omitted, it will autodetect current architecture.",
+    type=str,
+)
+parser.add_argument(
+    "-o",
+    "--operatingsystem",
+    help="Operating system. 'osx', 'linux' or 'windows'. If omitted, it will try to autodetect current OS.",
+    type=str,
+)
+parser.add_argument(
+    "-y", "--yes", help="Install even if version already installed", action="store_true"
+)
+parser.add_argument(
+    "-n", "--no", help="Don't install if version already installed", action="store_true"
+)
+parser.add_argument(
+    "-k", "--keep", help="Keep temporary downloaded archive file", action="store_true"
+)
+parser.add_argument(
+    "-r",
+    "--run",
+    help="Run downloaded Blender version when finished",
+    action="store_true",
+)
+parser.add_argument(
+    "-v", "--version", action="version", version="1.2", help="Print program version"
+)
 args = parser.parse_args()
 
 print(" SETTINGS ".center(80, "-"))
@@ -89,7 +115,13 @@ if os.path.isdir(args.path):
     dir_ = args.path
     print("Destination path: " + Fore.GREEN + args.path)
 else:
-    print(Fore.RED + "'" + args.path + "'" + " is an invalid path, make sure directory exists")
+    print(
+        Fore.RED
+        + "'"
+        + args.path
+        + "'"
+        + " is an invalid path, make sure directory exists"
+    )
     failed = True
 
 # check for desired blender version
@@ -113,11 +145,19 @@ elif not args.operatingsystem:
     if platform.system() == "Windows":
         opsys = "win"
         extension = "zip"
-        print("Operating system: " + Fore.GREEN + "Windows" + Fore.CYAN + " (autodetected)")
+        print(
+            "Operating system: "
+            + Fore.GREEN
+            + "Windows"
+            + Fore.CYAN
+            + " (autodetected)"
+        )
     elif platform.system() == "Linux":
         opsys = "linux"
         extension = "tar.bz2"
-        print("Operating system: " + Fore.GREEN + "Linux" + Fore.CYAN + " (autodetected)")
+        print(
+            "Operating system: " + Fore.GREEN + "Linux" + Fore.CYAN + " (autodetected)"
+        )
     elif platform.system() == "Darwin":
         opsys = "OSX"
         extension = "zip"
@@ -146,12 +186,16 @@ elif not args.architecture:
             failed = True
         else:
             arch = "32"
-            print("Architecture: " + Fore.GREEN + "32bit" + Fore.CYAN + " (autodetected)")
+            print(
+                "Architecture: " + Fore.GREEN + "32bit" + Fore.CYAN + " (autodetected)"
+            )
     elif "64" in platform.machine():
         arch = "64"
         print("Architecture: " + Fore.GREEN + "64bit" + Fore.CYAN + " (autodetected)")
 else:
-    print(Fore.RED + "Syntax error - please use '-a x86' for 32bit or '-a x64' for 64bit")
+    print(
+        Fore.RED + "Syntax error - please use '-a x86' for 32bit or '-a x64' for 64bit"
+    )
     failed = True
 
 # check for --keep flag
@@ -184,112 +228,137 @@ else:
     try:
         req = requests.get(url)
     except Exception:
-        print(Fore.RED + "Error connecting to " + url + ", check your internet connection")
+        print(
+            Fore.RED + "Error connecting to " + url + ", check your internet connection"
+        )
 
-    filename = re.findall(r'blender-' + blender + r'-\w+-' + opsys + r'[0-9a-zA-Z-._]*' + arch + r'\.' + extension, req.text)
+    filename = re.findall(
+        r"blender-"
+        + blender
+        + r"-\w+-"
+        + opsys
+        + r"[0-9a-zA-Z-._]*"
+        + arch
+        + r"\."
+        + extension,
+        req.text,
+    )
 
-    if os.path.isfile('./config.ini'):
-        config.read('config.ini')
-        if '2.81' in str(filename[0]):
+    if os.path.isfile("./config.ini"):
+        config.read("config.ini")
+        if "2.81" in str(filename[0]):
             try:
-                lastversion = config.get('main', 'version28')
+                lastversion = config.get("main", "version28")
             except Exception:
-                lastversion = ''
+                lastversion = ""
         if lastversion == filename[0]:
             while True:
                 if args.yes:
                     break
                 elif args.no:
-                    print("This version is already installed. -n option present, exiting...")
+                    print(
+                        "This version is already installed. -n option present, exiting..."
+                    )
                     sys.exit()
                 else:
-                    anyway = str(input('This version is already installed. Continue anyways? [Y]es or [N]o: ')).lower()
-                    if anyway == 'n':
+                    anyway = str(
+                        input(
+                            "This version is already installed. Continue anyways? [Y]es or [N]o: "
+                        )
+                    ).lower()
+                    if anyway == "n":
                         sys.exit()
-                    elif anyway == 'y':
+                    elif anyway == "y":
                         break
                     print("Invalid choice, try again!")
 
     else:
-        config.read('config.ini')
-        config.add_section('main')
-        with open('config.ini', 'w') as f:
+        config.read("config.ini")
+        config.add_section("main")
+        with open("config.ini", "w") as f:
             config.write(f)
 
-    if(not keep_temp):
-        if os.path.isdir('./blendertemp'):
-            shutil.rmtree('./blendertemp')
-    os.makedirs('./blendertemp', exist_ok=True)
-    dir_ = os.path.join(args.path, '')
+    if not keep_temp:
+        if os.path.isdir("./blendertemp"):
+            shutil.rmtree("./blendertemp")
+    os.makedirs("./blendertemp", exist_ok=True)
+    dir_ = os.path.join(args.path, "")
     print("Downloading " + filename[0])
     chunkSize = 10240
     try:
         r = requests.get(url + filename[0], stream=True)
-        with open("./blendertemp/" + filename[0], 'wb') as f:
-            pbar = IncrementalBar('Downloading', max=int(r.headers['Content-Length']) / chunkSize, suffix='%(percent)d%%')
+        with open("./blendertemp/" + filename[0], "wb") as f:
+            pbar = IncrementalBar(
+                "Downloading",
+                max=int(r.headers["Content-Length"]) / chunkSize,
+                suffix="%(percent)d%%",
+            )
             for chunk in r.iter_content(chunk_size=chunkSize):
                 if chunk:  # filter out keep-alive new chunks
                     pbar.next()
                     f.write(chunk)
             pbar.finish()
     except Exception:
-        print('Download' + Fore.RED + 'failed, please try again. Exiting.')
+        print("Download" + Fore.RED + "failed, please try again. Exiting.")
         sys.exit()
-    print('Download ' + Fore.GREEN + 'done')
+    print("Download " + Fore.GREEN + "done")
 
     # Extraction
-    spinnerExtract = Spinner('Extracting... ')
+    spinnerExtract = Spinner("Extracting... ")
     spinnerExtract.start()
     try:
-        shutil.unpack_archive("./blendertemp/" + filename[0], './blendertemp/')
+        shutil.unpack_archive("./blendertemp/" + filename[0], "./blendertemp/")
     except Exception:
-        print('Extraction ' + Fore.RED + 'failed, please try again. Exiting.')
+        print("Extraction " + Fore.RED + "failed, please try again. Exiting.")
         sys.exit()
     spinnerExtract.stop()
-    print('Extraction ' + Fore.GREEN + 'done')
+    print("Extraction " + Fore.GREEN + "done")
 
     # Copying
-    source = next(os.walk('./blendertemp/'))[1]
-    spinnerCopy = Spinner('Copying... ')
+    source = next(os.walk("./blendertemp/"))[1]
+    spinnerCopy = Spinner("Copying... ")
     spinnerCopy.start()
-    copy_tree(os.path.join('./blendertemp/', source[0]), dir_)
+    copy_tree(os.path.join("./blendertemp/", source[0]), dir_)
     spinnerCopy.stop()
-    print('Copying ' + Fore.GREEN + 'done')
+    print("Copying " + Fore.GREEN + "done")
 
     opsys = platform.system()
-    if opsys == 'darwin':
-        BlenderOSXPath = os.path.join('"' + dir_ + "\\blender.app/Contents/MacOS/blender" + '"')
+    if opsys == "darwin":
+        BlenderOSXPath = os.path.join(
+            '"' + dir_ + "\\blender.app/Contents/MacOS/blender" + '"'
+        )
         os.system("chmod +x " + BlenderOSXPath)
 
     # Cleanup
-    spinnerCleanup = Spinner('Cleanup... ')
+    spinnerCleanup = Spinner("Cleanup... ")
     spinnerCleanup.start()
-    if(keep_temp):
+    if keep_temp:
         # just remove the extracted files
-        shutil.rmtree(os.path.join('./blendertemp/', source[0]))
+        shutil.rmtree(os.path.join("./blendertemp/", source[0]))
     else:
-        shutil.rmtree('./blendertemp')
+        shutil.rmtree("./blendertemp")
 
     spinnerCleanup.stop()
-    print('Cleanup ' + Fore.GREEN + 'done')
+    print("Cleanup " + Fore.GREEN + "done")
 
     # Finished
     print("-".center(80, "-"))
     print(Fore.GREEN + "All tasks finished")
 
     # write configuration file
-    config.read('config.ini')
-    if '2.81' in str(filename[0]):
-        config.set('main', 'version28', filename[0])
-    with open('config.ini', 'w') as f:
+    config.read("config.ini")
+    if "2.81" in str(filename[0]):
+        config.set("main", "version28", filename[0])
+    with open("config.ini", "w") as f:
         config.write(f)
 
     # run Blender if -r flag present
     if args.run:
         print(Fore.MAGENTA + "Starting up Blender...")
-        if opsys == 'Windows':
+        if opsys == "Windows":
             p = subprocess.Popen(os.path.join('"' + dir_ + "\\blender.exe" + '"'))
-        elif opsys == 'darwin':
+        elif opsys == "darwin":
             p = subprocess.Popen(BlenderOSXPath)
-        elif opsys == 'Linux':
-            p = subprocess.Popen(os.path.join(dir_ + '/blender'))
+        elif opsys == "Linux":
+            p = subprocess.Popen(os.path.join(dir_ + "/blender"))
+
